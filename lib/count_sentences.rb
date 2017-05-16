8e27ddaf2168cf5295ba114cf9ea1a014643d819
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2017-05-16 19:20:54 -0400

    created monkey patch for String

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    count += self.split(" ").count{|string| string.end_with?(".") ||
+      string.end_with?("?") || string.end_with?("!")}
   end
 end
